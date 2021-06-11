739cbe76feab33f3fa45c78aa5b8026b19a8b75a
Pedro Sanders <sanderspedro@gmail.com>
2021-06-10 22:00:11 -0400

    Updated to support Javascript

--- templates/node12/base/utils/handlerManager.ts
+++ templates/node12/base/utils/handlerManager.ts
@@ -1,6 +1,6 @@
 import FunctionEvent from './functionEvent';
 import FunctionContext from './functionContext';
-import handler from '../function/handler';
+import handler from '../function/index';
 
 export default class HandlerManager {
     static async handler(functionEvent: FunctionEvent, functionContext:FunctionContext){
